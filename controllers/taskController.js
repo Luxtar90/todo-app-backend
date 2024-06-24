d96b76844378ac8aff14fdfb85ed6ed5e65979c3
Luxtar90 <luxtar9@gamil.com>
2024-06-23 22:45:18 -0500

    actualizacion del backend

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -2,7 +2,7 @@ const Task = require('../models/Task');
 
 exports.getTasks = async (req, res) => {
     try {
-        const tasks = await Task.find();
+        const tasks = await Task.find({ user: req.user.id });
         res.json(tasks);
     } catch (error) {
         console.error("Error al obtener tareas:", error);
@@ -12,7 +12,10 @@ exports.getTasks = async (req, res) => {
 
 exports.createTask = async (req, res) => {
     try {
-        const newTask = new Task(req.body);
+        const newTask = new Task({
+            user: req.user.id,
+            ...req.body
+        });
         const task = await newTask.save();
         res.json(task);
     } catch (error) {
@@ -45,6 +48,38 @@ exports.deleteTask = async (req, res) => {
         res.json({ message: 'Task deleted' });
     } catch (error) {
         console.error("Error al eliminar tarea:", error);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+
+exports.updateTaskStatus = async (req, res) => {
+    const { id } = req.params;
+    const { completed } = req.body;
+
+    try {
+        const task = await Task.findById(id);
+        if (!task) {
+            return res.status(404).json({ message: 'Task not found' });
+        }
+
+        task.completed = completed;
+        await task.save();
+
+        res.status(200).json(task);
+    } catch (error) {
+        console.error('Error updating task status:', error);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+
+const User = require('../models/User'); // Asegúrate de importar tu modelo de usuario
+
+exports.getProfile = async (req, res) => {
+    try {
+        const user = await User.findById(req.user._id).select('-password'); // Excluye la contraseña
+        res.json({ user });
+    } catch (error) {
+        console.error('Error fetching profile:', error);
         res.status(500).json({ message: 'Server error' });
     }
 };
